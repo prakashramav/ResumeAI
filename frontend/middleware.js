039f36b9a722867bfe3696dde0d3f8b71ebeb0e6
Arjun <arjunrathod7993@gmail.com>
2026-03-23 20:47:58 +0530

    renamed the file proxt to middleware

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,18 +1,16 @@
 import { NextResponse } from "next/server";
 
-export function proxy(req) {
+export function middleware(req) {
   const token = req.cookies.get("token")?.value;
   const { pathname } = req.nextUrl;
 
   const isAuthPage = pathname.startsWith("/auth");
   const isProtected = pathname.startsWith("/dashboard");
 
-  // If not logged in → block dashboard
   if (isProtected && !token) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
-  // If logged in → block auth pages ONLY
   if (isAuthPage && token) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
