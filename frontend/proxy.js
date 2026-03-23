dc40a56776bd26a81cf1339fe6d275dcc6baf632
Arjun <arjunrathod7993@gmail.com>
2026-03-23 20:02:29 +0530

    added proxy file for secure access

---
+++ frontend/proxy.js
@@ -0,0 +1,24 @@
+// proxy.js
+import { NextResponse } from "next/server";
+
+export function proxy(req) {
+  const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
+
+  const isAuthPage = pathname.startsWith("/auth");
+  const isProtected = pathname.startsWith("/dashboard");
+
+  if (isProtected && !token) {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
+  }
+  
+  if (isAuthPage && token) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/auth/:path*"],
+};
